eb9c4fdb91c0146c07124cb78855a1b8ded201d6
Huzaifa Khan <hk2785091@gmail.com>
2024-10-15 01:25:09 +0500

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path =require("path")
+
 //dotenv configuartion
 dotenv.config();
 
@@ -13,16 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 
-// static files
-app.use(express.static(path.join(__dirname,"./client/build")))
 
 
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
 
 //port
 const PORT = process.env.PORT || 5000;
